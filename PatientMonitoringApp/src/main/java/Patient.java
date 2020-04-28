f2bc32bf9f3bc9e1ae7830eca85f2d2f6718afd6
Chun Long Fong <cfon0005@student.monash.edu>
2020-04-28 16:02:37 +1000

    Extracting information

--- PatientMonitoringApp/src/main/java/Patient.java
+++ PatientMonitoringApp/src/main/java/Patient.java
@@ -12,10 +12,25 @@ public class Patient {
 
     public void getCholesterolInfo() throws IOException {
         JSONObject json = HttpService.readJsonFromUrl("https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/Patient/1?_format=json");
-        System.out.println(json.getString("gender"));
-        System.out.println(json.getJSONArray("name").getJSONObject(0));
+        JSONObject json2 = HttpService.readJsonFromUrl("https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/Observation?_count=5&code=2093-3&patient=1&_sort=-date&_format=json");
+
+        this.firstName = json.getJSONArray("name").getJSONObject(0).getJSONArray("given").getString(0);
+        System.out.println("FirstName:" + this.firstName);
+
         this.lastName = json.getJSONArray("name").getJSONObject(0).getString("family");
-        System.out.println(this.lastName);
-        System.out.println(json.toString());
+        System.out.println("LastName:" + this.lastName);
+
+        this.cholesterolValue = json2.getJSONArray("entry").getJSONObject(0).getJSONObject("resource").getJSONObject("valueQuantity").getFloat("value");
+        System.out.println("Cholesterol:" + this.cholesterolValue);
+
+        this.gender = json.getString("gender");
+        System.out.println("Gender:" + this.gender);
+
+        this.address = json.getJSONArray("address").getJSONObject(0).getJSONArray("line").getString(0) + " " +
+                "" + json.getJSONArray("address").getJSONObject(0).getString("city") + " " +
+                "" + json.getJSONArray("address").getJSONObject(0).getString("state")+ " " +
+                "" + json.getJSONArray("address").getJSONObject(0).getString("postalCode")+ " " +
+                "" + json.getJSONArray("address").getJSONObject(0).getString("country");
+        System.out.println("Address:" + this.address);
     }
 }
